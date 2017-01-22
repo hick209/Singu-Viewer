8bf2d5af10b1fabf23659ff1d338b7efe0ab6ffb
Nivaldo Bondanca <hick209@gmail.com>
2017-01-22 18:37:16 -0200

    Added skeletton to accept a request

--- app/requests/RequestsController.js
+++ app/requests/RequestsController.js
@@ -3,6 +3,8 @@
   angular.module('singu-viewer')
       .controller('RequestsController', [
         '$scope',
+        '$mdDialog',
+        '$mdToast',
         'AgendaService',
         'ApiService',
         RequestsController
@@ -10,6 +12,8 @@
 
   function RequestsController(
     $scope,
+    $mdDialog,
+    $mdToast,
     AgendaService,
     ApiService
   ) {
@@ -17,11 +21,27 @@
 
     AgendaService.configureAgenda($scope, viewModel, ApiService.getRequests)
     viewModel.requests = true;
+    viewModel.acceptRequest = acceptRequest;
 
     init();
 
     function init() {
       viewModel.refresh();
+    }
+
+    function acceptRequest(event, item) {
+      if (!item) return;
+
+      const confirm = $mdDialog.confirm()
+          .title(`Aceitar o pedido ${item.code}?`)
+          .textContent(`${item.client.name} - ${item.service} às ${moment(item.date).format('H:mm')}`)
+          .targetEvent(event)
+          .ok('Aceitar')
+          .cancel('Cancelar');
+
+      $mdDialog.show(confirm).then(() => {
+        $mdToast.show($mdToast.simple().textContent('Em breve!'));
+      })
     }
   }
 })();
